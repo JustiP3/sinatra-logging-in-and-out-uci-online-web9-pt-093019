99af1b8c09c19dfd6f4ae9232d3723232e5f9947
Justin Phillips <justinphil02@yahoo.com>
2020-02-01 04:22:47 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,8 @@
 class Helpers
-  def current_user
+  def current_user(session)
+    User.find(session[:user_id])
   end
-  def is_logged_in?
+  def is_logged_in?(session)
+    !!session[:user_id]
   end
 end
